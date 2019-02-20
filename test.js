d30184f517847dc68f4cd904389fcdc5ac076f96
nelsondominguez <nelsonjr73090@gmail.com>
2019-02-19 21:43:29 -0500

    Added average of 5 numbers

--- test.js
+++ test.js
@@ -1,14 +1,15 @@
 // JavaScript Document
 
-	function getAverage (a,b){
+	function getAverage (a,b,c,d,e,){
 
-		var average = (a + b) / 2;
+		var average = (a + b + c + d + e) / 5;
 
 			console.log(average);
-
+		
 		return average;
+
 	}
 
-	var myResult = getAverage(7,8);
+	var myResult = getAverage(7,8,9,10,11);
 
 		console.log("the average is" + myResult);
